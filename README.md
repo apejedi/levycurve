e44cd440e110d7bc5dd7804115a72f9d87024743
Jaideep Umraiya <jaideep.umraiya@gmail.com>
2014-03-03 21:00:37 -0500

    Adding matrix functions

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A program which generates levy curves with adjustable angles and iterations
 ## Installation
 Needs leinengen installed beforehand
-git clone git://github.com/apejedi/levycurve.git
+git clone https://github.com/apejedi/levycurve.git
 lein run
 
 ## Usage
--- src/levycurve/core.clj
+++ src/levycurve/core.clj
@@ -1,6 +1,8 @@
 (ns levycurve.core
   (:gen-class
    :name levycurve.core
+   (:import (java.awt.Dimension)
+            (java.lang.Math))
    :extends javax.swing.JFrame
    ))
 
@@ -9,3 +11,34 @@
    Initialize Data Structure"
   [& args]
   (println "Hello, World!"))
+
+(defn gen-matrix [theta]
+  (let [radians (Math/toRadians theta)
+        rotation [[(Math/cos radians) (Math/sin radians)]
+                  [(Math/sin radians) (Math/cos radians)]]
+        scale [[(/ 1 (Math/sqrt 2)) 0]
+               [0 (/ 1 (Math/sqrt 2))]]
+        mult-matrices (fn [m1 m2]
+                        (let [[a b c d] (flatten m1)
+                              [p q r s] (flatten m2)]
+                          [[(+ (* a p) (* b r)) (+ (* a q) (* b s))]
+                           [(+ (* c p) (* d r)) (+ (* c q) (* d s))]]
+                          ))
+        ]
+    (mult-matrices rotation scale)
+    )
+  )
+
+(defn gen-coord [p1 p2 matrix]
+        (let [[x1 y1] p1
+              [x2 y2] p2
+              x (Math/abs (- x2 x1))
+              y (Math/abs (- y2 y1)) ; Line relative to origin (0,0)
+              apply-matrix (fn [x y matrix]
+                             (let [[a1 a2 a3 a4] (flatten matrix)]
+                               [(+ (* x a1) (* y a2))
+                                (+ (* x a3) (* y a4))]))
+              [x y] (apply-matrix x y matrix)]
+          [(+ x x1) (+ y y1)]
+          )
+        )
